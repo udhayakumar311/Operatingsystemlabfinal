89446f08665551d60b23ad31ba810716fc672f02
udhayakumar311 <50603100+udhayakumar311@users.noreply.github.com>
2023-07-25 08:06:53 +0530

    Add files via upload

---
+++ Disk C-SCAN.cpp
@@ -0,0 +1,114 @@
+// C++ program to demonstrate
+// C-SCAN Disk Scheduling algorithm
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int n;
+int disk_size = 200;
+
+void CSCAN(int arr[], int head)
+{
+	int seek_count = 0;
+	int distance, cur_track;
+	vector<int> left, right;
+	vector<int> seek_sequence;
+
+	// appending end values
+	// which has to be visited
+	// before reversing the direction
+	left.push_back(0);
+	right.push_back(disk_size - 1);
+
+	// tracks on the left of the
+	// head will be serviced when
+	// once the head comes back
+	// to the beginning (left end).
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < head)
+			left.push_back(arr[i]);
+		if (arr[i] > head)
+			right.push_back(arr[i]);
+	}
+
+	// sorting left and right vectors
+	std::sort(left.begin(), left.end());
+	std::sort(right.begin(), right.end());
+
+	// first service the requests
+	// on the right side of the
+	// head.
+	for (int i = 0; i < right.size(); i++) {
+		cur_track = right[i];
+		// appending current track to seek sequence
+		seek_sequence.push_back(cur_track);
+
+		// calculate absolute distance
+		distance = abs(cur_track - head);
+
+		// increase the total count
+		seek_count += distance;
+
+		// accessed track is now new head
+		head = cur_track;
+	}
+
+	// once reached the right end
+	// jump to the beginning.
+	head = 0;
+
+	// adding seek count for head returning from 199 to 0
+	seek_count += (disk_size - 1);
+
+	// Now service the requests again
+	// which are left.
+	for (int i = 0; i < left.size(); i++) {
+		cur_track = left[i];
+
+		// appending current track to seek sequence
+		seek_sequence.push_back(cur_track);
+
+		// calculate absolute distance
+		distance = abs(cur_track - head);
+
+		// increase the total count
+		seek_count += distance;
+
+		// accessed track is now the new head
+		head = cur_track;
+	}
+
+	cout << "Total number of seek operations = "
+		<< seek_count << endl;
+
+	cout << "Seek Sequence is" << endl;
+
+	for (int i = 0; i < seek_sequence.size(); i++) {
+		if(i<=n){
+		cout << seek_sequence[i] << "->";
+	}
+	else{
+		cout << seek_sequence[i];
+	}
+	}
+}
+
+// Driver code
+int main()
+{
+	int h;
+	cout<<"Enter number of requests:";
+	cin>>n;
+	cout<<"Enter request sequence:";
+	int proc[n];
+	for(int i=0;i<n;i++){
+		cin>>proc[i];
+	}
+	cout<<"Enter initial head position:";
+	cin>>h;
+
+	CSCAN(proc, h);
+
+	return 0;
+}
